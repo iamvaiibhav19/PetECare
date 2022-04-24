863813c3a6869ef864d23cba8af7cd0d2e05bbbe
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2022-04-24 13:14:16 +0530

    user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -20,19 +20,19 @@ const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/password/forgot").post(forgotPassword);
-router.route("/logout").get(logoutUser);
-router.route("/password/reset/:token").put(resetPassword);
-router.route("/me").get(isAuthenticatedUser, getUserDetails);
-router.route("/password/update").put(isAuthenticatedUser, updatePassword);
-router.route("/me/update").put(isAuthenticatedUser, updateUserProfile);
-router
-  .route("/admin/user/:id")
-  .get(isAuthenticatedUser, authorisedRoles("admin"), getUser)
-  .put(isAuthenticatedUser, authorisedRoles("admin"), updateUserRole)
-  .delete(isAuthenticatedUser, authorisedRoles("admin"), deleteUser);
-router
-  .route("/admin/users")
-  .get(isAuthenticatedUser, authorisedRoles("admin"), getAllUsers);
+// router.route("/password/forgot").post(forgotPassword);
+// router.route("/logout").get(logoutUser);
+// router.route("/password/reset/:token").put(resetPassword);
+// router.route("/me").get(isAuthenticatedUser, getUserDetails);
+// router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+// router.route("/me/update").put(isAuthenticatedUser, updateUserProfile);
+// router
+//   .route("/admin/user/:id")
+//   .get(isAuthenticatedUser, authorisedRoles("admin"), getUser)
+//   .put(isAuthenticatedUser, authorisedRoles("admin"), updateUserRole)
+//   .delete(isAuthenticatedUser, authorisedRoles("admin"), deleteUser);
+// router
+//   .route("/admin/users")
+//   .get(isAuthenticatedUser, authorisedRoles("admin"), getAllUsers);
 
 module.exports = router;
